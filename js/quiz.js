994eab33db63872c68760c4d673b9f16ad27fd6e
Jip <jipvanbuitenen@gmail.com>
2021-09-16 21:47:23 +0200

    added comments, endpage, documentation and images

--- js/quiz.js
+++ js/quiz.js
@@ -10,6 +10,7 @@ function addButtonActions() {
     var ansButton2 = document.getElementById('button-ans2');
     var ansButton3 = document.getElementById('button-ans3');
     var correctAnsButton = document.getElementById('button-correctans');
+    var nextButton1 = document.getElementById('button-next');
 
     startButton.addEventListener("click", function () {
         showStartPage();
@@ -29,6 +30,9 @@ function addButtonActions() {
     ansButton3.addEventListener("click", function () {
         answrong3();
     });
+    nextButton1.addEventListener("click", function () {
+        showEndPage(); //CHANGE TO QUESTION 2
+    })
 }
 
 /**
@@ -37,11 +41,15 @@ function addButtonActions() {
 function hideAllPages() {
     var startPage = document.getElementById('page-start');
     var questionsPage = document.getElementById('page-questions');
-    var buttonNext = document.getElementById('button-next');
+    var buttonNext1 = document.getElementById('button-next');
+    var questionsPage2 = document.getElementById('page-questions2');
+    var endPage = document.getElementById('page-end');
 
     startPage.style.display = 'none';
     questionsPage.style.display = 'none';
-    buttonNext.style.display = 'none';
+    buttonNext1.style.display = 'none';
+    questionsPage2.style.display = 'none';
+    endPage.style.display = 'none';
 }
 
 /**
@@ -67,7 +75,7 @@ function showStartPage() {
 }
 
 /**
- * Show questions page
+ * Show questions page 1
  */
 function showQuestionsPage() {
     var page = document.getElementById('page-questions');
@@ -79,6 +87,35 @@ function showQuestionsPage() {
     console.info('Je bent nu op de vragenpagina');
 }
 
+/**
+ * Show questions page 2
+ */
+ function showQuestionsPage2() {
+    var page = document.getElementById('page-questions2');
+
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    console.info('Je bent nu op de vragenpagina deel 2');
+}
+
+/**
+ * Show end page
+ */
+ function showEndPage() {
+    var page = document.getElementById('page-end');
+
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    console.info('Je bent nu op de eindpagina');
+}
+
+/**
+ * correct ans button
+ */
 function anscorrect() {
     var correctAnsButton = document.getElementById('button-correctans');
     var buttonNext = document.getElementById('button-next');
@@ -88,6 +125,9 @@ function anscorrect() {
     console.info('correct');
 }
 
+/**
+ * wrong ans 1 button
+ */
 function answrong1() {
     var ansButton1 = document.getElementById('button-ans1');
     var correctAnsButton = document.getElementById('button-correctans');
@@ -100,6 +140,9 @@ function answrong1() {
     console.info('1 was wrong');
 }
 
+/**
+ * wrong ans 2 button
+ */
 function answrong2() {
     var ansButton2 = document.getElementById('button-ans2');
     var correctAnsButton = document.getElementById('button-correctans');
@@ -112,6 +155,9 @@ function answrong2() {
     console.info('2 was wrong');
 }
 
+/**
+ * wrong ans 3 button
+ */
 function answrong3() {
     var ansButton3 = document.getElementById('button-ans3');
     var correctAnsButton = document.getElementById('button-correctans');
